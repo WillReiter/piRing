8bec9e4652dd054666ca66cbacb55197c3326d74
Sam <sam.reiter88@gmail.com>
2020-12-24 14:03:30 -0600

    got clock.py working with color wipe

--- app/clock.py
+++ app/clock.py
@@ -8,6 +8,13 @@ import sys, os, time, datetime
 #            brightness=255, channel=0, strip_type=None, gamma=None)
 #            super.()
 
+def colorWipe(strip, color, wait_ms=100):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
 def read_settings():
     #import settings from YAML file
     #return session settings
@@ -20,11 +27,13 @@ def clock(params):
     # check for min/hour transition
     #   update LEDS or show animation
     # show
+    pass
 
 def static(params):
     # do static infinity mirror things here
     # pull functions straight from strandtest.py
     # take in a dict of animations & times to rotate?
+    pass
 
 def main():
     
@@ -37,11 +46,26 @@ def main():
     LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
     LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-    params = read_settings()
+    white = Color(127, 127, 127)
+    red = Color(255, 0, 0)
+    blue = Color(0, 255, 0)
+    green = Color(0, 0, 255)
+
+    #params = read_settings()
 
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 
     strip.begin()
+
+    colorWipe(strip, white, 50)
+        
+    colorWipe(strip, red, 50)
+
+    colorWipe(strip, blue, 50)
+
+    colorWipe(strip, green, 50)
+
+    colorWipe(strip, Color(0, 0, 0), 50)
 
     if clock:
         clock(params)
@@ -50,11 +74,12 @@ def main():
     elif game:
         wheelOfFortune()
 
-def systemboot()
+def systemboot():
     # check if WIFI is configured and connected
     # ping NTP to confirm that's all good
         # if not, fall back to AP mode + static pattern / status LEDs
     main()
+    pass
 
 if __name__ == "__main__":
     systemboot()
