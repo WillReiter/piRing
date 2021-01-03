37e0eb176d5670daae043aae5d13d58c1a032c95
WillReiter <will.reiter6@gmail.com>
2021-01-03 13:38:50 -0600

    added script to kill clock.py from it's PID by sending akeyboard interrupt  with pkill

---
+++ utils/killAppProc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys, os, signal
+
+# pkill signal values: https://linux.die.net/Bash-Beginners-Guide/sect_12_01.html
+
+bashCommand = "pkill -2 -f "
+scriptToKill = "clock.py"
+
+if len(sys.argv) > 1:
+    scriptToKill = sys.argv[1]
+
+fullCommand = bashCommand + scriptToKill
+
+print(fullCommand)
+
+subprocess.call(fullCommand, shell=True)
+
+# Catching KeyboardInterrupt in Python during program shutdown
+# https://stackoverflow.com/questions/21120947/catching-keyboardinterrupt-in-python-during-program-shutdown
+
+# https://stackoverflow.com/questions/13024532/simulate-ctrl-c-keyboard-interrupt-in-python-while-working-in-linux
